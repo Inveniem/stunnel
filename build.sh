e7f0a48c5e169642a8ebba76ce721ba206940397
Guy Elsmore-Paddock <guy@inveniem.com>
2024-03-25 00:15:34 -0400

    Update Build Scripts to Properly Detect Build Date and VCS Ref

--- build.sh
+++ build.sh
@@ -2,7 +2,23 @@
 
 source version.env
 
+##
+# Detects what commit of this project is checked-out.
+#
+# This information is exported to REPO_VCS_REF so it can be stamped into the
+# docker image.
+#
+detect_repo_commit() {
+  declare -g REPO_VCS_REF
+
+  REPO_VCS_REF=$(git rev-parse --short HEAD)
+}
+
+detect_repo_commit
+
 docker build \
+    --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+    --build-arg "VCS_REF=${REPO_VCS_REF}" \
     --build-arg "IMAGE_VERSION=${IMAGE_VERSION}" \
     --build-arg "STUNNEL_VERSION=${STUNNEL_VERSION}" \
     -t "inveniem/stunnel:${IMAGE_VERSION}" \
