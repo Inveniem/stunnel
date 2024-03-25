ce3163028e65ada3502a034fa4d23c70d685ce37
Guy Elsmore-Paddock <guy@inveniem.com>
2024-03-25 00:10:16 -0400

    Address Code Style Issues in Entry Point Script

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,8 @@ export STUNNEL_CRT="${STUNNEL_CRT:-/etc/stunnel/stunnel.pem}"
 # See: https://ssl-config.mozilla.org/
 export STUNNEL_CIPHERS="${STUNNEL_CIPHERS:-TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256}"
 
-if [[ -z "${STUNNEL_SERVICE}" ]] || [[ -z "${STUNNEL_ACCEPT}" ]] || \
-   [[ -z "${STUNNEL_CONNECT}" ]]; then
+if [ -z "${STUNNEL_SERVICE}" ] || [ -z "${STUNNEL_ACCEPT}" ] || \
+   [ -z "${STUNNEL_CONNECT}" ]; then
     {
         echo "one or more STUNNEL_* values missing: "
         echo "  STUNNEL_SERVICE=${STUNNEL_SERVICE}"
@@ -29,30 +29,30 @@ if [[ -z "${STUNNEL_SERVICE}" ]] || [[ -z "${STUNNEL_ACCEPT}" ]] || \
     exit 1
 fi
 
-if [[ ! -z "${STUNNEL_PSK}" ]]; then
-    if [[ ! -f ${STUNNEL_PSK} ]]; then
+if [ -n "${STUNNEL_PSK}" ]; then
+    if [ ! -f "${STUNNEL_PSK}" ]; then
         echo >&2 "STUNNEL_PSK (${STUNNEL_PSK}) must point to an existing file"
         exit 1
     fi
 
     TEMPLATE_SRC="stunnel.conf.psk.template"
 else
-    if [[ "${STUNNEL_CLIENT}" == "yes" ]]; then
+    if [ "${STUNNEL_CLIENT}" = "yes" ]; then
         TEMPLATE_SRC="stunnel.conf.pki-client.template"
     else
-        if [[ ! -f ${STUNNEL_KEY} ]]; then
-            if [[ -f ${STUNNEL_CRT} ]]; then
+        if [ ! -f "${STUNNEL_KEY}" ]; then
+            if [ -f "${STUNNEL_CRT}" ]; then
                 echo >&2 "crt (${STUNNEL_CRT}) missing key (${STUNNEL_KEY})"
                 exit 1
             fi
 
             openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-                -keyout ${STUNNEL_KEY} \
-                -out ${STUNNEL_CRT} \
+                -keyout "${STUNNEL_KEY}" \
+                -out "${STUNNEL_CRT}" \
                 -config /srv/stunnel/openssl.cnf
         fi
 
-        cp -v ${STUNNEL_CRT} /usr/local/share/ca-certificates/stunnel.crt
+        cp -v "${STUNNEL_CRT}" /usr/local/share/ca-certificates/stunnel.crt
 
         TEMPLATE_SRC="stunnel.conf.pki-server.template"
     fi
@@ -64,6 +64,6 @@ else
     update-ca-certificates 2>/dev/null
 fi
 
-cat "/srv/stunnel/${TEMPLATE_SRC}" | envsubst > ${STUNNEL_CONF}
+envsubst <"/srv/stunnel/${TEMPLATE_SRC}" >${STUNNEL_CONF}
 
 exec "$@"
